e0375e0e95ba2f5711592a9b0a03fa51fead23c0
Emir Taha Karcı <emirtahakarci12@gmail.com>
2023-04-27 21:58:13 +0300

    at

--- src/views/EmpProfileView.vue
+++ src/views/EmpProfileView.vue
@@ -1,4 +1,5 @@
 <template>
+  <emp-detail-card></emp-detail-card>
   <emp-profile-card></emp-profile-card>
   <default-card></default-card>
 </template>
@@ -6,9 +7,12 @@
 <script>
 import EmpProfileCard from "../components/EmployeeProfileComponents/EmpDashboardCard.vue";
 import DefaultCard from "../components/DashboardComponents/ProfileCard.vue";
+import EmpDetailCard from "../components/EmployeeProfileComponents/EmpDetailCard.vue";
+
 
 export default {
   components: {
+    EmpDetailCard,
     DefaultCard,
     EmpProfileCard
   },
