7542e5b29676e7b38e5b6fee5d6b1f970c26566a
Emir Taha Karcı <emirtahakarci12@gmail.com>
2023-06-01 16:22:37 +0300

    b

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createMemoryHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
-
 
 const routes = [
   {
@@ -73,7 +71,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createMemoryHistory(process.env.BASE_URL),
   routes
 })
 
